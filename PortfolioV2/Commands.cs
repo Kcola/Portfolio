27300e3153d662e5b1efc92cced25b6bb9b813b2
Kolawole Campbell <kolawole.campbell@gmail.com>
2020-10-12 23:32:05 -0400

    added vim command component.

---
+++ PortfolioV2/Commands.cs
@@ -0,0 +1,9 @@
+namespace PortfolioV2
+{
+    public static class Commands
+    {
+        public const string OpenFile = "e";
+        public const string Write = "w";
+        public const string Quit = "q";
+    }
+}
