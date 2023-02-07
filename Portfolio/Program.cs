0e75178d69bbd459010189067cbf2f96503a8f37
Kola Campbell <kolawole.campbell@gmail.com>
2023-02-06 23:40:10 -0500

    updated to .net 7

--- Portfolio/Program.cs
+++ Portfolio/Program.cs
@@ -1,12 +1,8 @@
 using System;
 using System.Net.Http;
-using System.Collections.Generic;
 using System.Threading.Tasks;
-using System.Text;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using Portfolio.Services;
 
 namespace Portfolio
@@ -19,7 +15,7 @@ namespace Portfolio
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(
-                sp => new HttpClient {BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)});
+                sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddStateProvider();
             builder.Services.AddJavaScriptInteropService();
 
